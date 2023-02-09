93a40ddf87a399d50bb0a6380353ae6ef2bd9004
XtianDev90 <chr.mondaca@gmail.com>
2023-02-08 21:46:58 -0300

    The project base is updated with the fixed bug so tha it stars connecting to a mongodb instance.

--- comercio/src/main/java/cl/christianmondaca/commerce/entity/TransactionEntity.java
+++ comercio/src/main/java/cl/christianmondaca/commerce/entity/TransactionEntity.java
@@ -1,13 +1,15 @@
 package cl.christianmondaca.commerce.entity;
 
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
 
 @Document(collation = "transactions")
-public class TransactionEntity{
+@Data
+public class TransactionEntity implements Comparable<TransactionEntity> {
     @Id
     private Long id;
     @Field
@@ -18,4 +20,9 @@ public class TransactionEntity{
     private FootEntity footEntity;
     @Field
     private AdditionalEntity additionalEntity;
+
+    @Override
+    public int compareTo(TransactionEntity o) {
+        return this.id.compareTo(o.id);
+    }
 }
